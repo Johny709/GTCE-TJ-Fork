c1e9e72131580620387fc3a4c5bb4a6a39911bda
Johny709 <janjoseph.merida@gmail.com>
2026-02-04 14:32:53 -0500

    add light to coils with CTM

--- src/main/java/gregtech/common/blocks/BlockFireboxCasing.java
+++ src/main/java/gregtech/common/blocks/BlockFireboxCasing.java
@@ -26,11 +26,6 @@ public class BlockFireboxCasing extends VariantBlock<FireboxCasingType> {
     }
 
     @Override
-    public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
-        return state.getValue(ACTIVE) ? 15 : 0;
-    }
-
-    @Override
     protected BlockStateContainer createBlockState() {
         super.createBlockState();
         return new BlockStateContainer(this, VARIANT, ACTIVE);
